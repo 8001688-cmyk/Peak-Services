556a3a5d4a98cbb7c26f4b8822c48d84e6731fe4
8001688 <8001688@schoolsnet.act.edu.au>
2026-08-27 13:44:00 +1000

    Started on Inventory and completed singup and login. Fix Navbar

--- Peak Services/main/urls.py
+++ Peak Services/main/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('signup/', views.signup, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 
  ]
